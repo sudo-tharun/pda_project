8d51580dbf2cb3b8f14bfb20f10540dbdb019633
Tharun B <67778546+sudo-tharun@users.noreply.github.com>
2021-11-11 23:39:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,10 @@
-#!/usr/bin/env python
+  
+mkdir -p ~/.streamlit/
 
-from pathlib import Path
-import subprocess
-import sys
-
-package_list = str(Path(Path.cwd() / 'requirements.txt'))
-
-# Upgrades pip
-def upgrade_pip():
-	subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
-
-
-# Calls pip to install a package referred using package_name.
-def install_package(package_name:str):
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', package_name])
-
-
-upgrade_pip()
-
-# Install packages using the file packages.txt
-package_names = open(package_list, 'r').readlines()
-
-for package in package_names:
-    install_package(package)
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
